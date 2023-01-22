c4a7799f05298d4024e66e1d900590a5055282fa
Fabiana Silva <fabianabz@yahoo.com>
2023-01-22 12:22:15 -0300

    aula 3 - frontend ok

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -23,7 +23,13 @@ export function SummaryTable() {
 			</div>
 			<div className="grid grid-rows-7 grid-flow-col gap-3">
 				{summaryDates.map((date) => {
-					return <HabitDay key={date.toString()} />;
+					return (
+						<HabitDay
+							key={date.toString()}
+							completed={Math.round(Math.random() * 5)}
+							amount={5}
+						/>
+					);
 				})}
 				{amountOfDaysToFull > 0 && Array.from({ length: amountOfDaysToFull }).map((_, index) => {
           return (
